c7a2fa87d0cfb886c69b7d3da298dc42c376b2f9
ResmiRamani <Resmi.RamaniAmma@triosstudent.com>
2019-10-15 09:47:00 -0400

    Added : My personalised message

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const fetch = require('node-fetch');
 
 console.log("hello world");
 cosole.log("Welcome");
+console.log("Hai,I am Resmi);
 fetch('https://google.com')
 .then(
 	  (result) => console.log(result)
